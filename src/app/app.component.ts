2d2ce5177f20ee51b8c209e977f46dcbca1a2cfd
Aleksandar Kitanovski <kitanovski.aleksandar@yahoo.com>
2024-03-24 01:57:59 +0100

    improve mobile support, add hamburger menu, minor fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,4 +14,26 @@ import { RouterModule } from '@angular/router';
 })
 export class AppComponent {
   title = 'portfolio-website';
+
+  toggle() {
+    let links = document.getElementById('links')!;
+    let navigation = document.getElementById('navigation')!;
+    let content = document.getElementById('content')!;
+
+    if (links.style.display == 'inline-block') {
+      links.style.display = 'none';
+
+      navigation.style.height = '50px';
+      navigation.style.flexDirection = 'row';
+
+      content.style.paddingTop = '50px';
+    } else {
+      links.style.display = 'inline-block';
+
+      navigation.style.height = '160px';
+      navigation.style.flexDirection = 'column';
+
+      content.style.paddingTop = '160px';
+    }
+  }
 }
